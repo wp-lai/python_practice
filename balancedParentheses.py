72314c194427af0de74f1746b69d8d64db53dbb4
wp-lai <techwplai@gmail.com>
2016-10-19 18:37:30 +0800

    update balanced parentheses

--- balancedParentheses.py
+++ balancedParentheses.py
@@ -21,7 +21,13 @@ False
 False
 >>> is_balanced("(()()(()")
 False
+
+>>> is_balanced("{{([][])}()}")
+True
+>>> is_balanced("[{()]")
+False
 """
+# Version 1: Only check '(' and ')'
 def is_balanced(str_):
     stack = []
     for char in str_:
@@ -42,6 +48,47 @@ def is_balanced(str_):
         return False
     else:
         return True
+
+
+# Version 2: check "([{" and ")]}"
+def is_balanced(str_):
+    stack = []
+    for char in str_:
+        if char in "([{":
+            stack.append(char)
+        elif char in ")]}":
+            if not stack:  # empty stack
+                return False
+            
+            top = stack[-1]
+            # check whether current char matches stack top
+            if matches(top, char):
+                stack.pop()
+            else:
+                return False
+        else:  # pass other characters
+            continue
+
+    # balanced if stack is empty
+    if stack:
+        return False
+    else:
+        return True
+
+
+def matches(char1, char2):
+    """Check whether open and close parenthesis matches
+
+        >>> matches("(", ")")
+        True
+        >>> matches("[", "]")
+        True
+        >>> matches("{", ")")
+        False
+    """
+    opens = "([{"
+    closes = ")]}"
+    return opens.index(char1) == closes.index(char2)
 
 
 if __name__ == '__main__':
