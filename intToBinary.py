08e568eb4093368583c24ba3a5414da077b2ba32
wp-lai <techwplai@gmail.com>
2016-10-19 19:28:40 +0800

    update intToBinary

--- intToBinary.py
+++ intToBinary.py
@@ -10,11 +10,23 @@ Converting decimal numbers to binary numbers
 
 >>> convert_to_binary(233)
 '0b11101001'
+
+>>> convert_to_binary(42)
+'0b101010'
+
+>>> convert_to_binary(0)
+'0b0'
+
+>>> convert_to_binary(1)
+'0b1'
 """
 # Solution 1: 
 # keep dividing by 2, store the remainder in a stack
 # read in reverse order
 def convert_to_binary(number):
+    if number == 0: 
+        return '0b0'
+
     stack = []
     while number:
         stack.append(number % 2)
@@ -28,6 +40,11 @@ def convert_to_binary(number):
 # Solution 2: using built-in function bin
 def convert_to_binary(number):
     return bin(number)
+
+
+# Solution 3: using string formatting
+def convert_to_binary(number):
+    return '0b{:b}'.format(number)
 
 
 if __name__ == '__main__':
