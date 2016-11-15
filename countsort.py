5e9cb4a85534652ffc1ff23c5b64c5eb2c48fad4
wp-lai <techwplai@gmail.com>
2016-11-15 21:07:41 +0800

    update count sort

--- countsort.py
+++ countsort.py
@@ -2,29 +2,33 @@
 Author: Alan Lai
 
 Task:
-Implements count sort, assumeing element is less than 10.
+Sorts integers from range [0, k)
 
->>> count_sort([4, 1, 3, 4, 3])
+>>> count_sort([4, 1, 3, 4, 3], 5)
 [1, 3, 3, 4, 4]
 """
 
 
-def count_sort(L):
-    k = 10  # assume element belongs to [0, 9)
+def count_sort(L, k):
+    # build a counter
     C = [0] * k
     for i in L:
         C[i] += 1
+
+    # build a cumulative counter
     C_cum = [0] * k
     for i in range(k):
         if i == 0:
             C_cum[i] = C[i]
         else:
             C_cum[i] = C_cum[i - 1] + C[i]
-    res = [None] * len(L)
+
+    # build output list
+    output = [None] * len(L)
     for j in reversed(L):
-        res[C_cum[j] - 1] = j
+        output[C_cum[j] - 1] = j
         C_cum[j] -= 1
-    return res
+    return output
 
 
 if __name__ == '__main__':
